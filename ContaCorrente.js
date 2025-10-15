7bc7e36417eecf8b4b923f1fd020c5c2aed66f99
Beatriz Cerqueira <beatrizcerqueira008@gmail.com>
2025-10-15 08:10:00 -0300

    Projeto classes

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,7 +1,30 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
     _cliente;
     _saldo = 0;
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this.saldo;
+    }
+
+    constructor(agencia, cliente) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 import {Cliente} from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = 'Ricardo';
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente('Ricardo', 11122233309);
 
-const contaCorrenteRicardo = new ContaCorrente;
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
+const cliente2 = new Cliente('Fabricia', 88822233309);
 
-const conta2 = new ContaCorrente;
-conta2.agencia = 102;
-conta2.cliente = new Cliente;
-conta2.cliente.nome = "Alice";
-conta2.cliente.cpf = 88822233309;
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+
+const conta2 = new ContaCorrente(102, cliente2);
 
 console.log(contaCorrenteRicardo._saldo);
 
 contaCorrenteRicardo.depositar(1000);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(-1);
-
-conta2.depositar(135);
-conta2.sacar(7);
 
 let valor = 200;
 const valorSacado = contaCorrenteRicardo.sacar(200);
 console.log(valorSacado);
 
 console.log(contaCorrenteRicardo);
-console.log(conta2);
+console.log(conta2.cliente);
 
 contaCorrenteRicardo.transferir(valor, conta2);
 console.log(contaCorrenteRicardo);
-console.log(conta2);
+console.log(ContaCorrente.numeroDeContas);
